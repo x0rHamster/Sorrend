e32525576ef42eef5449186fbde4c5f18f5734eb
Aleksandr Romanov <x0rhamster@ya.ru>
2022-07-21 17:06:22 +0300

    add solution skeleton

---
+++ src/Sorrend.MsBuildTool/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sorrend.MsBuildTool
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ tests/Sorrend.IntegrationTests/Startup.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Sorrend.IntegrationTests
+{
+    public static class Startup
+    {
+        public static void ConfigureServices(IServiceCollection services)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
